5081e9bdd0eed9920d0b3abffa0777eb2a42c51b
tony rafi <toni@pengembangan.lakuin.id>
2021-10-10 15:47:34 +0700

    clear code

--- app/src/main/java/com/template/project/AppConstants.java
+++ app/src/main/java/com/template/project/AppConstants.java
@@ -24,10 +24,7 @@ public class AppConstants {
 
     public static class CardId {
 
-        /**
-         * 目标金额内容
-         */
-        public static final int CARD_ID_PROJECT_MONEY_CONTENT_ID = 2001;
+
     }
 
     public static class RequestCode {
@@ -42,28 +39,17 @@ public class AppConstants {
         public final static int DEFAULT_REQ_CACHE_TIME = 60; //默认在线接口请求的缓存时间为60秒
 
         public static final int HTTP_CONNECTION_TIMEOUT = 15;
-        /**
-         * OkHttp3.2.0 bug https://github.com/square/okhttp/issues/1930,
-         * work around to set read timeout to 0
-         */
+
         public static final int HTTP_READ_TIMEOUT = 0;
         public static final int HTTP_WRITE_TIMEOUT = 15;
 
-        /**
-         * UserBean-Agent: Peduli/1.0 (Android5.0.1 API21)
-         * Qsc-Peduli-Token:
-         * Authorization: UID:Signature
-         * ExpireIn:
-         * Platform:Android
-         * DeviceID:
-         */
 
         public static final String HEADER_ACCEPT_KEY = "Accept";
         public static final String HEADER_ACCEPT_VALUE = "application/json";
         public static final String HEADER_ACCEPT_LANGUAGE = "Accept-Language";
 
         public static final String HEADER_USER_AGENT = "User-Agent";
-        public static final String HEADER_ACCESS_TOKEN = "Qsc-Peduli-Token";
+        public static final String HEADER_ACCESS_TOKEN = "App-Token";
         public static final String HEADER_AUTHORIZATION = "Authorization";
         public static final String HEADER_EXPIRES_IN = "ExpiresIn";
         public static final String HEADER_TOKEN_EXPIRES = "TokenExpires";
@@ -82,20 +68,11 @@ public class AppConstants {
 
 
 
-        /**
-         * PeduliSehat/1.1.5 (Android5.0.1 API21)
-         *
-         * @return
-         */
+
         public static String getUserAgent() {
-            return String.format("GudangView/%s (%s%s API%s)", BuildConfig.VERSION_NAME, HEADER_PLATFORM_VALUE, android.os.Build.VERSION.RELEASE, android.os.Build.VERSION.SDK_INT);
+            return String.format("App/%s (%s%s API%s)", BuildConfig.VERSION_NAME, HEADER_PLATFORM_VALUE, android.os.Build.VERSION.RELEASE, android.os.Build.VERSION.SDK_INT);
         }
 
-        /**
-         * 获取设备语言
-         *
-         * @return
-         */
         public static String getDeviceLan() {
             return LocaleUtil.getLanguage(MyApplication.getContext());
         }
